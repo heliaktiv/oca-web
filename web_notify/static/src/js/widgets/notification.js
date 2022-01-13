28c5be7dd1d77f78b90e929a544e1d29f0393dca
dejan.mirosavljevic <dejan.mirosavljevic@modoolar.com>
2022-01-13 13:37:48 +0100

    Custom changes in notification.js to skip custom configuration when in test mode

--- web_notify/static/src/js/widgets/notification.js
+++ web_notify/static/src/js/widgets/notification.js
@@ -11,8 +11,25 @@ odoo.define("web_notify.Notification", function(require) {
             info: "fa-info",
             default: "fa-lightbulb-o",
         },
+
+        are_we_in_test_mode: function() {
+            // This is a dirty little hack to figure out if we are in the test mode
+            return (
+                this.getParent() &&
+                this.getParent().$el &&
+                this.getParent().$el.length &&
+                this.getParent().$el[0].baseURI &&
+                this.getParent().$el[0].baseURI.includes("/web/tests")
+            );
+        },
+
         init: function() {
             this._super.apply(this, arguments);
+
+            // In case we are in test mode we want to skip custom configuration
+            // so we don't break Odoo base code tests
+            if (this.are_we_in_test_mode(arguments)) return;
+
             // Delete default classes
             this.className = this.className.replace(" o_error", "");
             // Add custom icon and custom class
